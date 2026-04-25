239237f43208e70fd5b5261e89cde66130599636
BashayerH <fo0oshaty@gmail.com>
2026-04-25 16:18:12 +0300

    finish_enhancement and ready to use#1

--- composeApp/src/commonMain/kotlin/com/example/memeeditor/meme_gallery/presentaion/MemeHomePage.kt
+++ composeApp/src/commonMain/kotlin/com/example/memeeditor/meme_gallery/presentaion/MemeHomePage.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.LayoutDirection
@@ -32,6 +33,7 @@ fun MemesScreen(
 
     onMemeSelected: (MemesTemplate) -> Unit,
 ) {
+    val displayTemplates = remember { memesListTemplates.shuffled() }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -54,7 +56,7 @@ fun MemesScreen(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
         ){
             items(
-                items = memesListTemplates,
+                items = displayTemplates,
                 key = {it.id}
             ){ memesTemplate ->
                 Card(
